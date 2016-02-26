0d911fb1f8f86af53f6221b394792d4990ba3360
Mahtab Syed <mahtab.syed@gmail.com>
2016-02-26 14:31:02 +0530

    Added the first page with a text message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+ def download
+    render text: "Work in progress... This page will have the button to download data for the products in store... Mahtab Syed"
+  end
+
 end
