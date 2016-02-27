6a8ccd68ada407766aaa3e21c0422f572652001c
Mahtab Syed <mahtab.syed@gmail.com>
2016-02-27 16:15:48 +0530

    Display Category and related Products

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,11 +6,13 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @products = @category.products.paginate(page: params[:page])
     #debugger
   end
 
   def index
-    @categories = Category.paginate(page: params[:page])
+    #@categories = Category.paginate(page: params[:page])
+    @categories = Category.order(:name).paginate(page: params[:page])
   end
 
 end
